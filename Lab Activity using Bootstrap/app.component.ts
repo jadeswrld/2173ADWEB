091ee988d0cc18b99e015a9066ef7b9b5fb1c3e9
jadeswrld <118106472+jadeswrld@users.noreply.github.com>
2024-01-14 19:50:08 +0800

    Add files via upload

---
+++ Lab Activity using Bootstrap/app.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  standalone: true
+})
+
+export class AppComponent {
+  title = 'This string is interpolated!';
+  clientName = 'JD';
+  appliedCSS = 'green';
+  notAppliedCSS = false;
+  myColor = 'red';
+
+   clickCount = 0
+    clickMe(){
+    this.clickCount++;
+  }
+
+  showData($event: any){
+    console.log('Button is clicked!');
+    if($event){
+      console.log($event.target);
+      console.log($event.target.value);
+    }
+  }
+}
